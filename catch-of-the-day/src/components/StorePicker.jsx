c19c7bcfb9c8669eaa460b7617d3cb8b8c5bebbb
Herve Desrosiers <hdesrosiers@stingray.com>
2018-10-30 07:38:11 -0400

    StorPicker component created

---
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>Dude!</p>;
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
-alert("dude");
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+
+render(<StorePicker />, document.getElementById("main")); //renders StorePicker
