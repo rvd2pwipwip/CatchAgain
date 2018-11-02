41913d70aeb10047977713850f0afde510b6d35c
Herve Desrosiers <hdesrosiers@stingray.com>
2018-11-02 07:50:10 -0400

    videos 11 and 12: Refs and event handlers

--- catch-of-the-day/src/components/StorePicker.jsx
+++ catch-of-the-day/src/components/StorePicker.jsx
@@ -1,11 +1,27 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = e => {
+    //stop the form from submitting
+    e.preventDefault();
+    //get text from input
+    const storeName = this.myInput.value.value;
+    //change url to /store/inputvalue
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.myInput}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
